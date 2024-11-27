1b9698b4ce31c4700162f4131a7c48b8a06c19b4
malwareMahmut <ozcelikeda18@gmail.com>
2024-11-27 13:56:16 +0300

    Add files via upload
    bunu gerçek bilgisiyarınızda açmayın

---
+++ yerli ve milli firus.bat
@@ -0,0 +1,159 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@echo off
+color a
+title Dark Sparrow
+
+echo Lütfen f11 e bas
+pause
+
+
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+start https :\\chromewebstore.google.com\detail\super-dark-mode\nlgphodeccbbcnkgmokeegopgpn
+
+echo F11 e bas.
+pause
+
+echo hayla etkilenmedinmi? Sen bilirsin.
+
+md DarkSparrow!
+md aa
+md a
+md aaa
+md a
+md b
+md c
+md df
+md f
+md eg
+md hghgh
+md cvbad fg
+md Ali koç istifa
+md Yönetim istifa
+md r353
+md vcb
+md 353
+md 5345
+md afı
+md sa
+md as
+md aha
+md ez
+md zor
+md gittdi
+md bittdi 
+md knk
+md gg
+md sadad
+md adadada
+md bittimi
+md simdi
+md son
+md birkez
+md yok
+md var
+md evettt
+md evettttttt
+md asdas
+md asds
+md Dark6
+md fake666664
+md azcık999
+md azcık99
+md azcık 9 
+md azcık 0
+md aaaaaaaaaaaaaaaaaaaaaaaaa
+md aaaaaaaaaa
+md aaaaaaaaaaaaaaaaaaaaaaaaaa
+md 0000000000000000000000000000000000000000000
+md 0000000000000000000000000000000000000000000000000 
+md 0000000000000000000
+md 01adana
+
+
+
+echo bukardı bays 
+pause
+
